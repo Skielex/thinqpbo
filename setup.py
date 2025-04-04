95aa7af8ffa53c0ac836c53c30d45d49f1f4683e
Niels Jeppesen <niejep@dtu.dk>
2025-04-04 13:46:37 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def extensions():
 
 setup(
     name="thinqpbo",
-    version="0.1.4",
+    version="0.1.5",
     author="Niels Jeppesen",
     author_email="niejep@dtu.dk",
     description="A thin QPBO wrapper for Python",
